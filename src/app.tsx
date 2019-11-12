76cdeaf4561e6a917ff153a21be2c27ad346cf11
Spawnbroker <eiche_jim@yahoo.com>
2019-11-11 21:18:47 -0500

    Setup eslint and prettier

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,10 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
 ReactDOM.render(
-    <div>Hello world again</div>,
-    document.getElementById('root')
+  <div>
+    Hello world again this is a really long string and I'd like prettier to
+    handle it for me
+  </div>,
+  document.getElementById("root")
 );
