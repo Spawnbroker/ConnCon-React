d5895e35589c4040ace4b4e47cf03d04518f9073
Spawnbroker <eiche_jim@yahoo.com>
2019-11-16 11:22:08 -0500

    Added Babel and Styled Components to webpack build

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,26 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import styled from "styled-components";
+
+// Create a Title component that'll render an <h1> tag with some styles
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
 
 ReactDOM.render(
-  <div>
-    Hello world again this is a really long string and I'd like prettier to
-    handle it for me
-  </div>,
+  <Wrapper>
+    <Title>
+      Hello world again this is a really long string and I'd like prettier to
+      handle it for me blah blah blah this line is way too long
+    </Title>
+  </Wrapper>,
   document.getElementById("root")
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
 module.exports = {
-    entry: './src/app.tsx',
-    output: {
-      path: __dirname + '/public',
-      filename: 'build/app.js'
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js']
-    },
-    module: {
-      rules: [
-        { test: /\.tsx?$/, loader: 'ts-loader' }
-      ]
-    }
+  entry: "./src/app.tsx",
+  output: {
+    path: __dirname + "/public",
+    filename: "build/app.bundle.js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        // Include ts, tsx, js, and jsx files.
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
   }
+};
